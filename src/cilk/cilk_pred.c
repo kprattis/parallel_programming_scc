700fa48f69e43f19e4b2c5093d13c83ca63803a9
kprattis <68779380+kprattis@users.noreply.github.com>
2022-11-27 02:07:48 +0200

    Add files via upload

---
+++ src/cilk/cilk_pred.c
@@ -0,0 +1,42 @@
+#include "cilk_scc.h"
+#include "cilk/cilk.h"
+
+void pred(graph *g, int* frontier, int* nscc){
+  
+    int frontier_is_empty = 0;
+	
+	cilk_for(int i = 0; i < g->n; i++){
+		if(frontier[i]){
+			g->removed[i] = 1;
+			g->scc[i] = nscc[g->colors[i]];
+		}
+	}
+	
+    while(!frontier_is_empty){
+        frontier_is_empty = 1;
+        
+        cilk_for(int i = 0; i < g->n; i++){
+            if(frontier[i]){
+	   			frontier[i] = 0;	
+		        int start = g->csr->ptr[i];
+		        int end = g->csr->ptr[i + 1];
+		   		
+		        cilk_for(int j = start; j < end; j++){
+					int parent = g->csr->ind[j];
+		            
+		            if(g->removed[parent]){
+		                continue;
+		            }
+		            
+		            if(g->colors[parent] == g->colors[i]){
+		                g->removed[parent] = 1;
+		                g->scc[parent] = nscc[g->colors[i]];
+		                frontier[parent] = 1;
+		                frontier_is_empty = 0;
+		            }
+		        }
+            }
+        }
+    }
+    
+}
---
+++ src/sparse.c
@@ -0,0 +1,57 @@
+#include "sparse.h"
+#include<stdio.h>
+
+void coo2csc(int *rows, int *cols, sparse *adj, int n, int nnz){
+	//assumes sorted cols	
+	for(int i = 0; i < nnz; i++){
+		adj->ind[i] = rows[i];
+		adj->ptr[cols[i]] ++;
+	}
+	//accumulative sum
+	int cum_sum = 0;
+	int temp;
+	for(int i = 0; i < n; i++){
+		temp = adj->ptr[i];
+		adj->ptr[i] = cum_sum;
+		cum_sum += temp;	
+	}
+	adj->ptr[n] = nnz;
+}
+
+void csc2csr(sparse *csc, sparse *csr, int n, int nnz){	
+	for(int i = 0; i < nnz; i++){			
+		csr->ptr[csc->ind[i]] ++;
+	}
+
+	//accumulative sum
+	int cum_sum = 0;
+	int temp;
+	for(int i = 0; i < n; i++){
+		temp = csr->ptr[i];
+		csr->ptr[i] = cum_sum;
+		cum_sum += temp;	
+		
+	}
+	csr->ptr[n] = nnz;
+		
+	int row, dest;
+	for(int i = 0; i < n; i++){
+		for(int j = csc->ptr[i]; j < csc->ptr[i + 1]; j++){				
+			row = csc->ind[j];
+			dest = csr->ptr[row];
+			csr->ind[dest] = i;
+			csr->ptr[row] ++;
+		}
+	}
+
+	int last = 0;
+	for(int i = 0; i < (n + 1); i++){
+		temp = csr->ptr[i];
+		csr->ptr[i] = last;
+		last = temp;
+	}
+
+}
+
+
+
