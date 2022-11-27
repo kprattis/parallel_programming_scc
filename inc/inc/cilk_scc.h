f2ebc7b3018682cc891795345fa5d284a91d230f
kprattis <68779380+kprattis@users.noreply.github.com>
2022-11-27 02:00:05 +0200

    Add files via upload

---
+++ inc/inc/cilk_scc.h
@@ -0,0 +1,16 @@
+#ifndef CILK_SCC_H
+#define CILK_SCC_H
+
+#include <stdlib.h>
+#include <stdlib.h>
+#include "graph.h"
+
+int scc(FILE* f, int **);
+
+void pred(graph *g, int* unique, int* nscc);
+
+int push_colors(graph *g);
+
+void trim(graph *g);
+
+#endif
